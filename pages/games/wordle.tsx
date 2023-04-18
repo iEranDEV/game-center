0ba538084489d9ec4879658c3ea42a3e81285103
Narei <ieranofficial@gmail.com>
2023-04-18 19:46:26 +0200

    wordle random word and word check

--- pages/games/wordle.tsx
+++ pages/games/wordle.tsx
@@ -2,31 +2,54 @@ import WordleBoardTile from "@/components/games/wordle/WordleBoardTile";
 import WordleKeyboard from "@/components/games/wordle/WordleKeyboard";
 import { WordleContext } from "@/context/WordleContext";
 import { useAnimate } from "framer-motion";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { AiOutlineLoading } from 'react-icons/ai';
 
 export default function Wordle() {
-    const [word, setWord] = useState('WORLD');
+    const [word, setWord] = useState<string | null>(null);
     const [userInput, setUserInput] = useState('');
     const [guesses, setGuesses] = useState(Array<string>());
 
     const [scope, animate] = useAnimate();
 
-    const handleUserInput = (key: string) => {
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchRandomWord = async () => {
+            const response = await fetch('https://random-word-api.herokuapp.com/word?length=5');
+            if(response.ok) {
+                const data = await response.json();
+                setWord(data[0].toUpperCase());
+            } else {
+                router.push('/');
+            }
+        }
+
+        fetchRandomWord();
+    }, [])
+
+    const handleUserInput = async (key: string) => {
         if(key === 'DELETE') {
             // User input backspace
             if(userInput.length > 0) {
                 setUserInput((prev) => prev.slice(0, -1));
             } else {
-                animate(scope.current, { x: [0, -10, 0, 10, 0]}, { duration: 0.2 })
+                animate(scope.current, { x: [0, -10, 0, 10, 0]}, { duration: 0.2 });
             }
 
         } else if(key === 'ENTER') {
             // User input enter
             if(userInput.length === 5) {
-                setGuesses([...guesses, userInput]);
-                setUserInput('');
+                const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + userInput.toLowerCase());
+                if(response.ok) {
+                    setGuesses([...guesses, userInput]);
+                    setUserInput('');
+                } else {
+                    animate(scope.current, { x: [0, -10, 0, 10, 0]}, { duration: 0.2 });
+                }
             } else {
-                animate(scope.current, { x: [0, -10, 0, 10, 0]}, { duration: 0.2 })
+                animate(scope.current, { x: [0, -10, 0, 10, 0]}, { duration: 0.2 });
             }
         } else {
             // User input letter
@@ -50,7 +73,7 @@ export default function Wordle() {
         return () => {
             document.removeEventListener('keydown', handleKeyDown);
         }
-    }, [userInput, guesses]);
+    }, [userInput, guesses, word]);
 
     const refProp = (row: number) => {
         if(row === guesses.length) return { ref: scope }
@@ -59,9 +82,7 @@ export default function Wordle() {
 
     return (
         <WordleContext.Provider value={{userInput, word, setUserInput, guesses, setGuesses, handleUserInput}}>
-            <div className='w-full h-full justify-between flex flex-col items-center'>
-                
-                <h1 className="py-2 font-bebas text-4xl">WORDLE</h1>
+            {word !== null ? <div className='w-full h-full justify-between flex flex-col items-center pt-10'>
 
                 {/* Wordle game board */}
                 <div className="h-4/6 flex flex-col gap-2">
@@ -76,7 +97,11 @@ export default function Wordle() {
 
                 <WordleKeyboard />
 
-            </div>
+            </div> : <>
+                <div className="w-full h-full flex justify-center items-center">
+                    <AiOutlineLoading className="h-8 w-8 animate-spin text-neutral-300"></AiOutlineLoading>
+                </div>
+            </>}
         </WordleContext.Provider>
     )
 }
