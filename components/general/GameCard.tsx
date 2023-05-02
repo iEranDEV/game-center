a6290ab512ba989738fef33b2d39835493d5d7fa
Narei <ieranofficial@gmail.com>
2023-05-02 17:33:17 +0200

    fixed game cards and added descriptions

--- components/general/GameCard.tsx
+++ components/general/GameCard.tsx
@@ -13,7 +13,7 @@ export default function GameCard({ game }: { game: GameType}) {
 
             <div className="w-full flex flex-col justify-between h-36 items-center p-2">
                 <h1 className="text-2xl text-primary">{game.title}</h1>
-                <span className="text-secondary">{game.description}</span>
+                <span className="text-secondary text-center">{game.description}</span>
                 <Button link={'/games/' + game.id} text={'play'}></Button>
             </div>
         </motion.div>
